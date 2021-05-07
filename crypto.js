fe09b841f0cd41bfdd55ba046be3c7b4cabb1939
Brian Schaaf <brianschaaf1@Brians-Mac-mini.attlocal.net>
2021-05-07 13:26:37 -0400

    made single function

--- crypto.js
+++ crypto.js
@@ -1,13 +1,15 @@
-const { pbkdf2Sync } = require('crypto');
+const { pbkdf2Sync, generateKeySync } = require('crypto');
 
 const generateMACaddress = (uniqueID) => {
-    const key = pbkdf2Sync( uniqueID, 'salt', 100000, 6, 'sha512');
-    return key.toString('hex')
-}
-const colonAdder = (generatedKey) => {
+    //generate random key for use incase no {uniqueID} was passed
+    const randomKey = generateKeySync('hmac', { length: 64 }).export().toString('hex');
+    //check to see if there was a uniqueId passed, if not use randomKey
+    let idString = !uniqueID ? randomKey : uniqueID
+    // hash randomly generated key or uniqueID
+    const generatedKey = pbkdf2Sync( idString, 'salt', 100000, 6, 'sha512').toString('hex');
+    //return hash in MAC format
     let keyToArray = generatedKey.split('');
     let result = [];
-
     let count = 0;
     for (let i = 0; i < keyToArray.length; i++) {
         count++;
@@ -22,10 +24,10 @@ const colonAdder = (generatedKey) => {
     }
     return result.join('');
 }
-let test = () => {
+    let test = () => {''
     console.log('Hashed iPad uniqueID: ',generateMACaddress('68864E4A-D09E-4D03-BCDF-7899BA502F81'))
-    console.log('key length is 12: ',generateMACaddress('68864E4A-D09E-4D03-BCDF-7899BA502F81').length === 12)
-    console.log('return hash in MAC format: ',colonAdder('bb08dd0b7447'))
-    console.log('should be a string: ',typeof colonAdder(generateMACaddress('68864E4A-D09E-4D03-BCDF-7899BA502F81')) === 'string')
+    let passedString1 = generateMACaddress('68864E4A-D09E-4D03-BCDF-7899BA502F81')
+    let passedString2 = generateMACaddress('68864E4A-D09E-4D03-BCDF-7899BA502F81')
+    console.log('When passed same uniqueID returns same MAC address: ', passedString1 === passedString2)
 }
 test()
